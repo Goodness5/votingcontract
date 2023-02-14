2edce0213e58a5731cd388ab673332c259eaee90
superman <goodnesskolapo@gmail.com>
2023-02-14 16:45:44 +0100

    puuushhh

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -4,7 +4,7 @@ import { ethers } from "hardhat";
 async function main() {
   const [owner, admin1, admin2, admin3] = await ethers.getSigners();
   const contestants = [admin1, admin2, admin2]
-  console.log(owner.address, admin1.address, admin2.address, admin3.address);
+  console.log(contestants);
 
 
 
@@ -14,20 +14,20 @@ async function main() {
 
   console.log(`Multisig Address is ${vote.address}`);
 
-  const newadmin = await vote.addAdmin(admin1.address);
+  // const newadmin = await vote.addAdmin(admin1.address);
 
-  await vote.transfer(admin2.address, 100);
+  // await vote.transfer(admin2.address, 100);
 
 
 
-  const candidates = await vote["registerCandidate(address)"];
-  console.log(candidates);
+  // const candidates = await vote["registerCandidate(address)"];
+  // console.log(candidates);
 
-  await vote.vote(admin3.address, 3);
+  // await vote.vote(admin3.address, 3);
 
-  await vote.vote(admin1.address, 5);
+  // await vote.vote(admin1.address, 5);
 
-  await vote.vote(admin2.address, 7);
+  // await vote.vote(admin2.address, 7);
 
 
 
