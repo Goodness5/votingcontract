1b4bf9cdef4dfe2cbd0057ce3b5dfe51d2188609
superman <goodnesskolapo@gmail.com>
2023-02-14 06:11:03 +0100

    damn

--- scripts/vote.ts
+++ scripts/vote.ts
@@ -1,8 +1,27 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [owner, admin1] = await ethers.getSigners();
-  const admin = [owner.address, admin1.address];
+  const [owner, admin1, admin2, admin3] = await ethers.getSigners();
+  const contestants = [admin1, admin2, admin2]
+  console.log(owner.address, admin1.address, admin2.address, admin3.address);
+
+
+
+  const Vote = await ethers.getContractFactory("Vote");
+  const vote = await Vote.deploy("SUPERMAN","SG");
+  await vote.deployed();
+
+  console.log(`Multisig Address is ${vote.address}`);
+
+  const addAmin = await vote.addAdmin(admin1.address);
+  
+
+
+
+
+
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
